cf09d163c7768f9be95573bbd20aa058683dac37
Jonathan Fuentes <john.fuen95@gmail.com>
2024-06-09 23:10:39 -0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component"
 import './App.css';
 
 class App extends Component {
@@ -9,20 +11,15 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
         this.setState(
           () => {
             return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
           }
         )
       );
@@ -37,7 +34,6 @@ class App extends Component {
   }
 
   render() {
-    console.log('render');
 
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
@@ -47,19 +43,8 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="Search Monsters"
-          onChange={onSearchChange}
-        />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monsters'} className="search-box"/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
